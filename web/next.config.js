cf84a7157435da88cf1ab2b054b81939e5af0f41
Emily Perin <emily.perin2@gmail.com>
2023-06-27 19:05:16 -0300

    fix: next config

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'localhost'],
+    domains: ['avatars.githubusercontent.com', 'localhost', '192.168.0.105'],
   },
 }
 
